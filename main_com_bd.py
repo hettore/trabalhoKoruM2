5ae53d18fb04830cc1853f3463c22998d6da5e42
Hettore Eduardo <duh.barretoo@gmail.com>
2023-08-11 10:14:30 -0300

    Melhorando api, separando algumas funções

--- main_com_bd.py
+++ main_com_bd.py
@@ -2,40 +2,16 @@ from flask import Flask, jsonify, request, json
 import sqlite3
 import os
 
+import repository_com_bd
+
 app = Flask(__name__)
-
-caminho = f"{os.path.dirname(__file__)}\\db\\produtos.db"
-
-#Função para gerar um novo id
-def gerar_id():
-    conn = sqlite3.connect(caminho)
-    cursor = conn.cursor()
-    cursor.execute("SELECT seq FROM sqlite_sequence WHERE name='produtos'")
-    next_id = cursor.fetchone()[0]
-    return next_id + 1
 
 #Criar um novo produto no banco
 #nome, preco, peso, descricao, fornecedor
 @app.route('/product', methods=['POST'])
 def criar_produto():
     try:
-        obj = request.json
-        values = (obj['nome'], obj['preco'], obj['peso'], obj['descricao'], obj['fornecedor'])
-        conn = sqlite3.connect(caminho)
-        cursor = conn.cursor()
-        sql_insert = "INSERT INTO produtos (nome_produto, preco_produto, peso_produto, descricao_produto, fornecedor_produto) values (?, ?, ?, ?, ? )"
-        cursor.execute(sql_insert, (values))
-        obj_id = cursor.lastrowid
-        produto = {
-                "id":obj_id,
-                "nome": obj['nome'],
-                "preco": obj['preco'],
-                "peso":obj['peso'],
-                "descricao": obj['descricao'],
-                "fornecedor": obj['fornecedor']
-                }
-        conn.commit()
-        conn.close()
+        produto = repository_com_bd.criando_produto()
         
         return jsonify(produto), 201
     except Exception as ex:
@@ -46,24 +22,8 @@ def criar_produto():
 @app.route('/', methods=['GET'])
 def retornar_produtos():
     try:
-        resultado = []
-        conn = sqlite3.connect(caminho)
-        cursor = conn.cursor()
-        sql_select = "SELECT * FROM produtos"
-        cursor.execute(sql_select)
-        produtos = cursor.fetchall()
-        conn.close()
-        for item in produtos:
-            produtos = {
-                         'id': item[0],
-                         'nome': item[1],
-                         'preco': item[2],
-                         'peso': item[3],
-                         'descricao': item[4],
-                         'fornecedor': item[5]
-                       }
-            resultado.append(produtos)
-        return resultado
+        resultado = repository_com_bd.retornando_produtos()
+        return resultado, 200
     except:
         return False 
     
@@ -71,74 +31,31 @@ def retornar_produtos():
 @app.route('/product/<int:id>', methods=['GET'])
 def retornar_produto(id:int):
     try:
-        # if id == 0:
-        #     return jsonify({'Mensagem': "Produto não encontrado"})
-        conn = sqlite3.connect(caminho)
-        cursor = conn.cursor()
-
-        sql_select = "SELECT * FROM produtos WHERE id_produto = ?"
-        cursor.execute(sql_select, (id, ))
-        id, nome, preco, peso, descricao, fornecedor = cursor.fetchone()
-        conn.close()
-        #prod = id, nome, preco, peso, descricao, fornecedor
-        return {
-                "id":id,
-                "nome": nome,
-                "preco": preco,
-                "peso": peso,
-                "descricao": descricao,
-                "fornecedor": fornecedor
-                }
+        produto = repository_com_bd.retornando_produto(id)
+        return produto, 200
     except:
         return jsonify({'Mensagem': "Produto não encontrado"}), 404
     
 #Atualiza os dados de um produto
-def atualizar_produto(id:int, nome, preco, peso, descricao, fornecedor):
-    try:
-        #Tentar atualizar
-        conn = sqlite3.connect(caminho)
-        cursor = conn.cursor()
-        sql_update = "UPDATE produtos SET nome_produto = ?, preco_produto = ?, peso_produto = ?, descricao_produto = ?, fornecedor_produto = ? WHERE id_produto = ?"
-        cursor.execute(sql_update, (nome, preco, peso, descricao, fornecedor, id))
-        conn.commit()
-        conn.close()
-        return True
-    except Exception as ex:
-        print(ex)
-        return False
 @app.route('/product/<int:id>', methods=['PUT'])
 def atualizar_produto_id(id):
     try:
-        produto = retornar_produto(id)
+        produto = repository_com_bd.retornando_produto(id)
         if produto['id'] == id:
             dados_atualizados = request.json
             dados_atualizados["id"]=id
-            atualizar_produto(**dados_atualizados)
+            repository_com_bd.atualizar_produto(**dados_atualizados)
             return jsonify(dados_atualizados), 200
     except Exception:
-        return jsonify({'Mensagem': "Produto não encontrado"})
+        return jsonify({'Mensagem': "Produto não encontrado"}), 404
 
-    
-#Remove um produto
-def remover_produto(id:int):
-    try:
-        conn = sqlite3.connect(caminho)
-        cursor = conn.cursor()
-        sql_delete = "DELETE FROM produtos WHERE id_produto = ?"
-        cursor.execute(sql_delete, (id, ))
-        conn.commit()
-        conn.close()
-        return True
-    except Exception as ex:
-        print(ex)
-        return False
-    
+#Deletando um produto
 @app.route('/product/<int:id>', methods=['DELETE'])
 def deletar_produto(id):
     try:
-        produto = retornar_produto(id)
+        produto = repository_com_bd.retornando_produto(id)
         if produto:
-            remover_produto(id)
+            repository_com_bd.remover_produto(id)
             return jsonify({"Mensagem": "produto deletado com sucesso"}, produto)
     except Exception:
         return jsonify({"Mensagem": "Produto não encontrado"}), 404
@@ -150,6 +67,3 @@ def deletar_produto(id):
 
 
 app.run(debug=True)
-
-
-
---
+++ repository_com_bd.py
@@ -0,0 +1,101 @@
+from flask import request, jsonify
+import sqlite3
+import os
+
+caminho = f"{os.path.dirname(__file__)}\\db\\produtos.db"
+
+#Função para gerar um novo id
+def gerar_id():
+    conn = sqlite3.connect(caminho)
+    cursor = conn.cursor()
+    cursor.execute("SELECT seq FROM sqlite_sequence WHERE name='produtos'")
+    next_id = cursor.fetchone()[0]
+    return next_id + 1
+
+def criando_produto():
+        obj = request.json
+        values = (obj['nome'], obj['preco'], obj['peso'], obj['descricao'], obj['fornecedor'])
+        conn = sqlite3.connect(caminho)
+        cursor = conn.cursor()
+        sql_insert = "INSERT INTO produtos (nome_produto, preco_produto, peso_produto, descricao_produto, fornecedor_produto) values (?, ?, ?, ?, ? )"
+        cursor.execute(sql_insert, (values))
+        obj_id = cursor.lastrowid
+        produto = {
+                "id":obj_id,
+                "nome": obj['nome'],
+                "preco": obj['preco'],
+                "peso":obj['peso'],
+                "descricao": obj['descricao'],
+                "fornecedor": obj['fornecedor']
+                }
+        conn.commit()
+        conn.close()
+        return produto
+
+def retornando_produtos():
+        resultado = []
+        conn = sqlite3.connect(caminho)
+        cursor = conn.cursor()
+        sql_select = "SELECT * FROM produtos"
+        cursor.execute(sql_select)
+        produtos = cursor.fetchall()
+        conn.close()
+        for item in produtos:
+            produtos = {
+                         'id': item[0],
+                         'nome': item[1],
+                         'preco': item[2],
+                         'peso': item[3],
+                         'descricao': item[4],
+                         'fornecedor': item[5]
+                       }
+            resultado.append(produtos)
+        return resultado
+
+def retornando_produto(id:int):
+        # if id == 0:
+        #     return jsonify({'Mensagem': "Produto não encontrado"})
+        conn = sqlite3.connect(caminho)
+        cursor = conn.cursor()
+
+        sql_select = "SELECT * FROM produtos WHERE id_produto = ?"
+        cursor.execute(sql_select, (id, ))
+        id, nome, preco, peso, descricao, fornecedor = cursor.fetchone()
+        conn.close()
+        #prod = id, nome, preco, peso, descricao, fornecedor
+        return {
+                "id":id,
+                "nome": nome,
+                "preco": preco,
+                "peso": peso,
+                "descricao": descricao,
+                "fornecedor": fornecedor
+                }
+
+def atualizar_produto(id:int, nome, preco, peso, descricao, fornecedor):
+    try:
+        #Tentar atualizar
+        conn = sqlite3.connect(caminho)
+        cursor = conn.cursor()
+        sql_update = "UPDATE produtos SET nome_produto = ?, preco_produto = ?, peso_produto = ?, descricao_produto = ?, fornecedor_produto = ? WHERE id_produto = ?"
+        cursor.execute(sql_update, (nome, preco, peso, descricao, fornecedor, id))
+        conn.commit()
+        conn.close()
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
+    
+    #Remove um produto
+def remover_produto(id:int):
+    try:
+        conn = sqlite3.connect(caminho)
+        cursor = conn.cursor()
+        sql_delete = "DELETE FROM produtos WHERE id_produto = ?"
+        cursor.execute(sql_delete, (id, ))
+        conn.commit()
+        conn.close()
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
